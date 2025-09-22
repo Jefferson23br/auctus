8ce2ce7c62b25a94d7f266622dfe123d2b49203a
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-09-22 20:30:12 -0300

    correção

--- Reembolso-Km/backend/index.js
+++ Reembolso-Km/backend/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-
 const authRoutes = require('./routes/authRoutes');
 const veiculoRoutes = require('./routes/veiculoRoutes');
 const viagemRoutes = require('./routes/viagemRoutes');
@@ -19,10 +18,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+const corsOptions = {
+  origin: 'https://www.auctusconsultoria.com.br'
+};
+app.use(cors(corsOptions));
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('API do Reembolso de Km - Telsign está no ar!');
@@ -36,6 +40,7 @@ app.use('/api/upload', uploadRoutes);
 app.use('/api/pagamentos', pagamentoRoutes); 
 app.use('/api/dashboard', dashboardRoutes); 
 app.use('/api/relatorios', relatorioRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
