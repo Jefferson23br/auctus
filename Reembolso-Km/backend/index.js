d0a6ce894d0ea20ba5fccf8287b43ace9a834f02
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-09-30 20:04:49 -0300

    Update index.js

--- Reembolso-Km/backend/index.js
+++ Reembolso-Km/backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors'); // 1. Importar o pacote cors
 
 const authRoutes = require('./routes/authRoutes');
 const veiculoRoutes = require('./routes/veiculoRoutes');
@@ -15,6 +15,8 @@ const relatorioRoutes = require('./routes/relatorioRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
