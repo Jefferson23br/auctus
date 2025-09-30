48ff60b4efb0e3ce19cfb3056046423884bfcbae
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-09-30 19:16:33 -0300

    Update authController.js

--- Reembolso-Km/backend/controllers/authController.js
+++ Reembolso-Km/backend/controllers/authController.js
@@ -87,7 +87,7 @@ exports.forgotPassword = async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST, 
       port: process.env.EMAIL_PORT, 
-      secure: false, 
+      secure: true, 
       auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS, 
