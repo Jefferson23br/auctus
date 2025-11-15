de61c61df0aa23d2ef61a341004046804040e711
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-11-15 00:06:58 -0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -8,16 +8,16 @@
             <h4>Contato</h4>
             <p><strong>WhatsApp:</strong> <a href="https://wa.me/5535999187047" target="_blank">+55 (35) 99918-7047</a></p>
             <p><strong>E-mail:</strong> contato@auctusconsultoria.com.br</p>
-            <p><strong>Cidade/UF:</strong> <!-- coloque aqui sua cidade/estado --></p>
+            <p><strong>Localização:</strong> Ribeirão Preto - São Paulo, Brasil</p>
         </div>
         <div class="footer-col">
             <h4>Links</h4>
             <ul>
-                <li><a href="/quem-somos.php">Quem Somos</a></li>
-                <li><a href="/como-podemos-te-ajudar.php">Como Podemos te Ajudar</a></li>
-                <li><a href="/servicos.php">Serviços</a></li>
-                <li><a href="/portfolio.php">Portfólio</a></li>
-                <li><a href="/contato.php">Contato</a></li>
+                <li><a href="quem-somos.php">Quem Somos</a></li>
+                <li><a href="como-podemos-te-ajudar.php">Como Podemos te Ajudar</a></li>
+                <li><a href="servicos.php">Serviços</a></li>
+                <li><a href="portfolio.php">Portfólio</a></li>
+                <li><a href="contato.php">Contato</a></li>
             </ul>
         </div>
     </div>
@@ -26,7 +26,6 @@
         <p>Desenvolvido por Auctus Consultoria.</p>
     </div>
 </footer>
-
 <script src="assets/js/scripts.js"></script>
 </body>
 </html>
