8eb20872edff4a2cd57ccb9b43576f6abe800154
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2026-04-23 13:06:36 -0300

    Update google-tag.php

--- google-tag.php
+++ google-tag.php
@@ -1,9 +1,10 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-67534BPNQ6"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18114654835"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
+  gtag('config', 'AW-18114654835');
   gtag('config', 'G-67534BPNQ6');
 </script>
